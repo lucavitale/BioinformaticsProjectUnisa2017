10b727c7b351f39fc17fabab885c7a5fe8f7a5f7
Gioele Ciaparrone <julianbrennan93@gmail.com>
2017-01-30 12:12:38 +0100

    readData2

--- readData.R
+++ readData.R
@@ -4,7 +4,16 @@ readData <- function() {
   fileExprsGenes <- file.path(getwd(), "all_genes.txt"); fileExprsGenes
   fileExprsMirnas <- file.path(getwd(), "all_mirnas.txt"); fileExprsMirnas
   filePhenodata <- file.path(getwd(), "patient_classes.txt"); filePhenodata
-  geneData <- readExpressionSet(fileExprsMirnas, phenoDataFile = filePhenodata, exprsArgs = list(), phenoDataArgs = list(quote="\""))
+  mirnaData <- readExpressionSet(fileExprsMirnas, phenoDataFile = filePhenodata, exprsArgs = list(), phenoDataArgs = list(quote="\""))
 }
 
 readData()
+
+readData2 <- function() {
+  fileExprsGenes <- file.path(getwd(), "all_genes.txt"); fileExprsGenes
+  fileExprsMirnas <- file.path(getwd(), "all_mirnas.txt"); fileExprsMirnas
+  filePhenodata <- file.path(getwd(), "patient_classes.txt"); filePhenodata
+  
+  mirnaTable <- as.matrix(read.table(fileExprsMirnas))
+  phenoData <- read.table(filePhenodata)
+}
