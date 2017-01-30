1cf2f97fd9777ef2ff20a4aa62297f8fd120f4e9
Gioele Ciaparrone <julianbrennan93@gmail.com>
2017-01-30 17:05:40 +0100

    Fixed reading data

--- mergeData.R
+++ mergeData.R
@@ -1,4 +1,4 @@
 files <- as.data.frame(list.files("BreastMaleData"))
-fm <- read.table("file_matching.txt", header = TRUE)
+fm <- read.table("file_matching.txt", header = TRUE, row.names = NULL, sep = "\t")
 names(files) <- "file_id"
 merged <- merge(files, fm, by = "file_id")
