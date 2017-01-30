2f5b9181689ee08e113989e80cb8db7b91892221
Gioele Ciaparrone <julianbrennan93@gmail.com>
2017-01-30 11:44:20 +0100

    Removed call to browser

--- readData.R
+++ readData.R
@@ -4,7 +4,6 @@ readData <- function() {
   fileExprsGenes <- file.path(getwd(), "all_genes.txt"); fileExprsGenes
   fileExprsMirnas <- file.path(getwd(), "all_mirnas.txt"); fileExprsMirnas
   filePhenodata <- file.path(getwd(), "patient_classes.txt"); filePhenodata
-  browser()
   geneData <- readExpressionSet(fileExprsMirnas, phenoDataFile = filePhenodata, exprsArgs = list(), phenoDataArgs = list(quote="\""))
 }
 readData()
