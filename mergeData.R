f2744deeaad4fcc102fbc3d509db05e3153aa7b9
TheAvenger93 <lucavitale93@yahoo.it>
2017-01-30 17:15:55 +0100

    fix mergeData

--- mergeData.R
+++ mergeData.R
@@ -1,4 +1,4 @@
 files <- as.data.frame(list.files("BreastMaleData"))
-fm <- read.table("file_matching.txt", header = TRUE, row.names = NULL, sep = "\t")
+fm <- read.table("file_matching.txt", header = TRUE, sep = "\t")
 names(files) <- "file_id"
 merged <- merge(files, fm, by = "file_id")
