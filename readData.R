2ed1599b58232ad44fa969b1c4f71ef58c5c16a7
Gioele Ciaparrone <julianbrennan93@gmail.com>
2017-01-30 12:17:54 +0100

    Fixed reading data

--- readData.R
+++ readData.R
@@ -4,16 +4,13 @@ readData <- function() {
   fileExprsGenes <- file.path(getwd(), "all_genes.txt"); fileExprsGenes
   fileExprsMirnas <- file.path(getwd(), "all_mirnas.txt"); fileExprsMirnas
   filePhenodata <- file.path(getwd(), "patient_classes.txt"); filePhenodata
-  mirnaData <- readExpressionSet(fileExprsMirnas, phenoDataFile = filePhenodata, exprsArgs = list(), phenoDataArgs = list(quote="\""))
-}
-
-readData()
-
-readData2 <- function() {
-  fileExprsGenes <- file.path(getwd(), "all_genes.txt"); fileExprsGenes
-  fileExprsMirnas <- file.path(getwd(), "all_mirnas.txt"); fileExprsMirnas
-  filePhenodata <- file.path(getwd(), "patient_classes.txt"); filePhenodata
   
   mirnaTable <- as.matrix(read.table(fileExprsMirnas))
   phenoData <- read.table(filePhenodata)
+  mirnaTable <- mirnaTable[,which(colnames(mirnaTable) %in% rownames(phenoData))]
+  phenoData <- AnnotatedDataFrame(phenoData)
+  
+  mirnaSet <- ExpressionSet(mirnaTable, phenoData = phenoData)
 }
+
+readData()
