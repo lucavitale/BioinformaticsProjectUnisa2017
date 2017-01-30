485fd7d5a8e2c0b73ad0723aee4aa3c88069c9da
Gioele Ciaparrone <julianbrennan93@gmail.com>
2017-01-30 11:42:38 +0100

    Updated readData function

--- readData.R
+++ readData.R
@@ -1,5 +1,10 @@
-fileExprsGenes <- file.path(getwd(), "all_genes.txt"); fileExprsGenes
-fileExprsMirnas <- file.path(getwd(), "all_mirnas.txt"); fileExprsMirnas
-filePhenodata <- file.path(getwd(), "patient_classes.txt"); filePhenodata
-browser()
-geneData <- readExpressionSet(fileExprsMirnas, phenoDataFile = filePhenodata, exprsArgs = list(), phenoDataArgs = list())
+library(DFP)
+
+readData <- function() {
+  fileExprsGenes <- file.path(getwd(), "all_genes.txt"); fileExprsGenes
+  fileExprsMirnas <- file.path(getwd(), "all_mirnas.txt"); fileExprsMirnas
+  filePhenodata <- file.path(getwd(), "patient_classes.txt"); filePhenodata
+  browser()
+  geneData <- readExpressionSet(fileExprsMirnas, phenoDataFile = filePhenodata, exprsArgs = list(), phenoDataArgs = list(quote="\""))
+}
+readData()
