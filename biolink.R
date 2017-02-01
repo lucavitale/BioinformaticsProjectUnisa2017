a2434b8bb9327850efe28c83ea84ecf0ba1b8a0a
TheAvenger93 <lucavitale93@yahoo.it>
2017-02-01 10:24:51 +0100

    Add genes expression e fix biolink file

--- biolink.R
+++ biolink.R
@@ -1,8 +1,7 @@
 library(TCGAbiolinks)
 
-RNAq <- GDCquery(project = "TCGA-BRCA", data.category = "Transcriptome Profiling",data.type = "Gene Expression Quantification", workflow.type = "HTSeq - FPKM")
-GDCdownload(RNAq)
-
+RNAq <- GDCquery(project = "TCGA-BRCA", data.category = "Transcriptome Profiling",data.type = "Gene Expression Quantification", workflow.type = "HTSeq - FPKM-UQ")
+GDCdownload(RNAq,chunks.per.download= 150)
 
 miRNAq <- GDCquery(project = "TCGA-BRCA", data.category = "Transcriptome Profiling",data.type = "miRNA Expression Quantification", workflow.type = "BCGSC miRNA Profiling")
-GDCdownload(miRNAq)
+GDCdownload(miRNAq, chunks.per.download= 250)
