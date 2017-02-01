ae833d1ec346469a1b4628290b60cbd0b0c67c43
TheAvenger93 <lucavitale93@yahoo.it>
2017-02-01 17:23:17 +0100

    fix biolink

--- biolink.R
+++ biolink.R
@@ -1,7 +1,11 @@
 library(TCGAbiolinks)
+library(SummarizedExperiment)
 
 RNAq <- GDCquery(project = "TCGA-BRCA", data.category = "Transcriptome Profiling",data.type = "Gene Expression Quantification", workflow.type = "HTSeq - FPKM-UQ")
 GDCdownload(RNAq,chunks.per.download= 150)
 
 miRNAq <- GDCquery(project = "TCGA-BRCA", data.category = "Transcriptome Profiling",data.type = "miRNA Expression Quantification", workflow.type = "BCGSC miRNA Profiling")
 GDCdownload(miRNAq, chunks.per.download= 250)
+
+RNATable <-  GDCprepare(RNAq,mut.pipeline = NULL)
+miRNATable <- GDCprepare(miRNAq,mut.pipeline = NULL)
