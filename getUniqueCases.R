5943a6ab38234cbdbfa0e1042c2f1e37eb49ac32
Gioele Ciaparrone <julianbrennan93@gmail.com>
2017-02-02 15:17:12 +0100

    Write output

--- getUniqueCases.R
+++ getUniqueCases.R
@@ -2,3 +2,7 @@ t <- read.table("file_matching.txt", header = TRUE, sep = "\t")
 tRNA <- t[which(t$experimental_strategy=="RNA-Seq"),]
 tmiRNA <- t[which(t$experimental_strategy=="miRNA-Seq"),]
 tmerged <- intersect(unique(tRNA$cases_0_submitter_id), unique(tmiRNA$cases_0_submitter_id))
+tRNA2 <- tRNA[which(tRNA$cases_0_submitter_id %in% tmerged),]
+tmiRNA2 <- tmiRNA[which(tRNA$cases_0_submitter_id %in% tmerged),]
+write.table(tRNA2, file = "RNA_patients.txt", sep = "\t")
+write.table(tmiRNA2, file = "miRNA_patients.txt", sep = "\t")
