1abad95f5c6a5d3671ef1adf528e0e8994c0ffbd
TheAvenger93 <lucavitale93@yahoo.it>
2017-02-02 11:59:34 +0100

    fix convert genes

--- Convert_genes.R
+++ Convert_genes.R
@@ -37,10 +37,10 @@ RNAByAliquot <- aggregate(. ~ genes,FUN = median, data=RNAByAliquot)
 
 save(RNAByAliquot,file = "RNAByAliquot.Rdata")
 
-## Changed Barcode with 
+## Changed Barcode with patient
 barCode <- colData(RNATable)[,2:3]
 names(RNAByAliquot) <- c("genes",colData(RNATable)$patient)
 row.names(RNAByAliquot) <- RNAByAliquot$genes
 RNAByAliquot <- RNAByAliquot[,-1]
 
-
+## Remove same patients
