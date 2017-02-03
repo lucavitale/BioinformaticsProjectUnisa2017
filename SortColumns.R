9c2dfe11d27286d557e0812182eda64536f7a556
TheAvenger93 <lucavitale93@yahoo.it>
2017-02-03 10:01:52 +0100

    Change col names of patients

--- SortColumns.R
+++ SortColumns.R
@@ -4,11 +4,17 @@ load("RNAFinal.Rdata")
 load("miRNAPatientsFinal.Rdata")
 load("miRNAFinal.Rdata")
 
+## Order
 RNAFinal <- RNAFinal[,order(names(RNAFinal))]
 miRNAFinal <- miRNAFinal[,order(names(miRNAFinal))]
 
 RNAPatientsFinal <- RNAPatientsFinal[order(RNAPatientsFinal$cases_0_submitter_id),]
 miRNAPatientsFinal <- miRNAPatientsFinal[order(miRNAPatientsFinal$cases_0_submitter_id),]
+
+## Change columns names
+colPat <- c("caseID","sampleType","daysTolastFollowUp","fileName","siteOfResectionOrBiopsy","fileID","morphology","primaryDiagnosis","submitterID","vitalStatus","aliquotsSubmitterID","daysToDeath","daysToBirth","ageAtDiagnosis","tissueOrOrganOfOrigin","tumorStage","experimentalStrategy","stageClass")
+names(miRNAPatientsFinal) <- colPat
+names(RNAPatientsFinal) <- colPat
 
 save(RNAPatientsFinal,file = "RNAPatientsFinal.Rdata")
 save(RNAFinal, file = "RNAFinal.Rdata")
