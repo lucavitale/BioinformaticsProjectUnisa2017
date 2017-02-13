c01d525f90b861379f7e2e74e09c184678795c61
TheAvenger93 <lucavitale93@yahoo.it>
2017-02-13 17:54:44 +0100

    added script for MAPlot

---
+++ MAPlot.R
@@ -0,0 +1,11 @@
+rnasel <- RNAFinal[rowSums(RNAFinal)!=0,]
+rnanorm =log2(rnasel+8)
+
+range(rnanorm)
+# stage i vs stage i
+plot((rnanorm[,3]+rnanorm[,7])/2, (rnanorm[,3]-rnanorm[,7]),xlim=c(0,26))
+# stage iii vs stage i
+plot((rnanorm[,4]+rnanorm[,7])/2, (rnanorm[,4]-rnanorm[,7]),xlim=c(0,26))
+# stage iii vs stage i
+plot((rnanorm[,4]+rnanorm[,3])/2, (rnanorm[,4]-rnanorm[,3]),xlim=c(0,26))
+
