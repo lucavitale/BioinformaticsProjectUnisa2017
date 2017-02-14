ba4e09a72ffa48fef7f23e692d53f0315123f493
TheAvenger93 <lucavitale93@yahoo.it>
2017-02-14 12:32:06 +0100

    DFP for brca2norm

--- AdjustStage.R
+++ AdjustStage.R
@@ -35,3 +35,25 @@ stage[which(stage=="stage iiic")] <- "stage iii"
 RNAPatients$stage <- stage
 
 save(RNAPatients, file="RNA_patients.Rdata")
+
+## BRCA2norm adjustment
+patientsBRCA2norm <- read.table("PatientsBRCA2norm",sep="\t",header=T)
+stage <- patientsBRCA2norm$diagnoses_0_tumor_stage
+unique(stage)
+
+stage[which(stage=="stage ia")] <- "stage i"
+stage[which(stage=="stage ib")] <- "stage i"
+
+stage[which(stage=="stage iia")] <- "stage ii"
+stage[which(stage=="stage iib")] <- "stage ii"
+
+stage[which(stage=="stage iiia")] <- "stage iii"
+stage[which(stage=="stage iiib")] <- "stage iii"
+stage[which(stage=="stage iiic")] <- "stage iii"
+
+patientsBRCA2norm$stage <- stage
+
+patientsBRCA2norm <- patientsBRCA2norm[which(patientsBRCA2norm$stage %in% c("stage i","stage ii","stage iii")),]
+patientsBRCA2norm <- droplevels(patientsBRCA2norm)
+
+save(patientsBRCA2norm, file="BRCA2_RNA_patients.Rdata")
--- MAPlot.R
+++ MAPlot.R
@@ -4,8 +4,7 @@ rnanorm =log2(rnasel+8)
 range(rnanorm)
 # stage i vs stage i
 plot((rnanorm[,3]+rnanorm[,7])/2, (rnanorm[,3]-rnanorm[,7]),xlim=c(0,26))
-# stage iii vs stage i
+v# stage iii vs stage i
 plot((rnanorm[,4]+rnanorm[,7])/2, (rnanorm[,4]-rnanorm[,7]),xlim=c(0,26))
 # stage iii vs stage i
 plot((rnanorm[,4]+rnanorm[,3])/2, (rnanorm[,4]-rnanorm[,3]),xlim=c(0,26))
-
