cc415970d16586c888620bd9ef03449229458ec2
Juggernaut93 <julianbrennan93@gmail.com>
2017-02-14 18:02:45 +0100

    Added custom discretizeExpressionValues with sapply function instead of for cycle.

---
+++ discretizeExpressionValues.R
@@ -0,0 +1,37 @@
+discretizeExpressionValues <- function(rmadataset, mfs, zeta = 0.5, overlapping = 2) 
+{
+  rmam <- exprs(rmadataset)
+  rmam[c(1:8), c(1:4)]
+  rmav <- as.vector(pData(phenoData(rmadataset))$class)
+  rmav
+  names(rmav) <- sampleNames(rmadataset)
+  rmav
+  gene.names <- rownames(rmam)
+  gene.names
+  dvs <- NULL
+  #for (ig in gene.names) {
+  #  values <- rmam[ig, ]
+  #  values
+  #  disc.values <- .fuzzyDiscretization(mfs[[ig]]$lel, mfs[[ig]]$mel, 
+  #                                      mfs[[ig]]$hel, values, zeta, overlapping)
+  #  disc.values
+  #  dvs <- rbind(dvs, disc.values)
+  #  dvs
+  #}
+  
+  doit <- function(ig) {
+    #browser()
+    values <- rmam[ig, ]
+    DFP:::.fuzzyDiscretization(mfs[[ig]]$lel, mfs[[ig]]$mel, 
+                         mfs[[ig]]$hel, values, zeta, overlapping)
+    #return(c(ig, disc.values))
+  }
+  
+  dvs <- t(sapply(gene.names, doit))
+  
+  rownames(dvs) <- gene.names
+  head(dvs)
+  attr(dvs, "types") <- rmav
+  dvs
+  return(dvs)
+}
