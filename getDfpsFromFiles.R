0c784f44d39ea63cb6b1c1aba6edf3c8dc1def62
Juggernaut93 <julianbrennan93@gmail.com>
2017-02-16 16:55:46 +0100

    Added script to obtain data from DFP result files.

---
+++ getDfpsFromFiles.R
@@ -0,0 +1,16 @@
+getDfpsFromFiles <- function(idx) {
+  res <- list()
+  for(i in idx) {
+    fn <- paste("paramList", i, ".Rdata", sep = "")
+    load(fn)
+    res[i] <- list(
+      skipFactor = paramList$skipFactor,
+      zeta = paramList$zeta,
+      piVal = paramList$piVal,
+      overlapping = paramList$overlapping,
+      dfps = paramList$dfps,
+      numGenes = nrow(paramList$dfps)
+    )
+  }
+  return(res)
+}
