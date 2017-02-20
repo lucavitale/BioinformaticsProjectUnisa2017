42f2de2dcc9651e43bf1ff1cb1527ceba556aed8
Juggernaut93 <julianbrennan93@gmail.com>
2017-02-20 18:40:26 +0100

    Fixed reading paramList with only 1 gene

--- getDfpsFromFiles.R
+++ getDfpsFromFiles.R
@@ -21,6 +21,11 @@ getDfpResults <- function(idx){
   for(i in idx) {
     fn <- paste("paramList", i, ".Rdata", sep = "")
     load(fn)
+    if(class(paramList$dfps) == "character") {
+      dfps2 <- matrix(paramList$dfps, nrow = 1)
+      attr(dfps2, "ifs") <- attr(paramList$dfps, "ifs")
+      paramList$dfps <- dfps2
+    }
     df = rbind(df,data.frame(paramList$skipFactor, paramList$zeta, paramList$piVal, paramList$overlapping, nrow(paramList$dfps),fn))
     print(paste(i, "done."))
   }
