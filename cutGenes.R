a24319370fd5b464e22000be88904d733eb2e3cc
TheAvenger93 <lucavitale93@yahoo.it>
2017-02-21 16:54:26 +0100

    Organized Data

--- cutGenes.R
+++ cutGenes.R
@@ -1,3 +1,5 @@
 varThreshold <- 10
 
 rnanormvar <- rnanorm[apply(rnanorm, 1, var) > 10,]
+idx = which(rf$importance>0.6)
+rf2 <- randomForest(x = t(rnanormvar[idx,]),y = RNAPatientsFinal$stageClass)
