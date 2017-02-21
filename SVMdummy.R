9934af7cf013a01e5cc0df7bdfac2e42085e8b66
TheAvenger93 <lucavitale93@yahoo.it>
2017-02-21 16:06:42 +0100

    fix bug

--- SVMdummy.R
+++ SVMdummy.R
@@ -1,9 +1,16 @@
-nvar = 1000
+library(e1071)
+#nvar = 1000
 
-index <- 1:nrow(Rsvm)
-testindex <- sample(index, trunc(length(index)/3))
-testset <- Rsvm[testindex,c(1:nvar,ncol(Rsvm))]
-trainset <- Rsvm[-testindex,c(1:nvar,ncol(Rsvm))]
-svm.model <- svm(classes ~ ., data = trainset, cost = Inf, gamma = 1)
-svm.pred <- predict(svm.model, testset[,-(nvar+1)])
-table(pred = svm.pred, true = testset[,nvar+1])
+#index <- 1:nrow(Rsvm)
+#testindex <- sample(index, trunc(length(index)/3))
+#testset <- Rsvm[testindex,c(1:nvar,ncol(Rsvm))]
+#trainset <- Rsvm[-testindex,c(1:nvar,ncol(Rsvm))]
+
+trainset <- as.data.frame(t(rnanorm[rownames(paramList$dfps),trainingIdx]))
+trainset$classes <- RNAPatientsFinal[trainingIdx,"stageClass"]
+testset <- as.data.frame(t(rnanorm[rownames(paramList$dfps),-trainingIdx]))
+testset$classes <- RNAPatientsFinal[-trainingIdx,"stageClass"]
+
+svm.model <- svm(classes ~ ., data = trainset, cost = 1e12, gamma = 1, kernel="radial")
+svm.pred <- predict(svm.model, testset[,-(ncol(testset))])
+table(pred = svm.pred, true = testset[,(ncol(testset))])
