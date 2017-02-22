f3512c92ec3abf26b666385bc8a668ba1b3c7628
TheAvenger93 <lucavitale93@yahoo.it>
2017-02-22 16:53:34 +0100

    Generated pathways for oxford dataset

--- createPathwayDataset.R
+++ createPathwayDataset.R
@@ -1,8 +1,9 @@
-load("RNANORM.RData")
-load("RNAPatientsFinal")
+oxfordGenes <- read.table("oxford/genes.txt",sep="\t",header = T)
+oxfordClass <- read.table("oxford/patient_classes.txt",sep="\t",header = T)
 
 load("reactomeFinal.Rdata")
 load("keggFinal.Rdata")
+#load("biocartaFinal.Rdata")
 
 load("trainingIdx.RData")
 
@@ -15,11 +16,11 @@ names = row.names(pathways)
 for(i in 1:nrow(pathways)){
   cur <- pathways[i,]
   genes <- strsplit(cur$geneID,"/")[[1]]
-  data = t(rnanorm[genes,trainingIdx])
-  write.table(data,file=paste("pathways/",names[i],".txt",sep=""),quote=F)
+  data = t(oxfordGenes[genes,trainingIdx])
+  write.table(data,file=paste("oxfordPathways/",names[i],".txt",sep=""),quote=F)
 }
 
-class <- as.data.frame(as.integer(RNAPatientsFinal[trainingIdx,]$stageClass))
+class <- as.data.frame(as.integer(oxfordClass[trainingIdx,]$stageClass))
 names(class) <- "x"
-row.names(class) <- rownames(RNAPatientsFinal[trainingIdx,])
+row.names(class) <- rownames(oxfordClass[trainingIdx,])
 write.table(class,file="pathways/labels",quote=F)
--- enrichment.R
+++ enrichment.R
@@ -1,5 +1,17 @@
 library(GSA)
-gmtfile <- system.file("extdata", "msigdb.v5.2.entrez.gmt",package="clusterProfiler")
+library(clusterProfiler)
+
+load("oxfordDFPResults/paramList60.Rdata")
+gene <- rownames(paramList$dfps)
+breast_genes <- read_csv("breast_genes.txt", col_names = FALSE)
+
+breast_genes <- breast_genes$X1[breast_genes$X1 %in% rownames(paramList$fps)]
+
+gene <- union(gene,c("BRCA1","BRCA2"))
+gene <- union(gene,breast_genes)
+
+
+gmtfile <- system.file("extdata", "msigdb.v5.2.symbols.gmt",package="clusterProfiler")
 c2kegg <- read.gmt(gmtfile)
 
 #sum(gene %in% c2kegg$gene)
@@ -13,15 +25,18 @@ reactome <- egmt@result[grep(pattern = "reactome",x = rownames(egmt@result),igno
 kegg <- egmt@result[grep(pattern = "kegg",x = rownames(egmt@result),ignore.case = T),]
 biocarta <- egmt@result[grep(pattern = "biocarta",x = rownames(egmt@result),ignore.case = T),]
 
-gmtfile <- system.file("extdata", "c6.all.v5.2.entrez.gmt",package="clusterProfiler")
-c6 <- read.gmt(gmtfile)
-c6 <- unique(c6$ont)
+#gmtfile <- system.file("extdata", "c6.all.v5.2.entrez.gmt",package="clusterProfiler")
+#c6 <- read.gmt(gmtfile)
+#c6 <- unique(c6$ont)
 
 
 reactomeFinal <- reactome[which(reactome$Count >=10),]
 keggFinal <- kegg[which(kegg$Count >=10),]
-c6Final <- egmt@result[egmt@result$ID %in% c6 & egmt@result$Count >= 10,]
+biocartaFinal <- biocarta[which(biocarta$Count >=10),]
+
+#c6Final <- egmt@result[egmt@result$ID %in% c6 & egmt@result$Count >= 10,]
 
 save(reactomeFinal,file="reactomeFinal.Rdata")
 save(keggFinal,file="keggFinal.Rdata")
-save(c6Final,file="c6Final.Rdata")
+#save(biocartaFinal,file="biocartaFinal.Rdata")
+#save(c6Final,file="c6Final.Rdata")
